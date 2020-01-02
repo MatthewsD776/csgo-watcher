15f289732da38e347dd16ec2540c5bbf4edc8547
Darren Matthews <darren.matthews@citrix.com>
2020-01-02 14:15:03 +0000

    Analyses the payload and stores the game data in an object, detects when the game has ended.

--- GameServer/server.js
+++ GameServer/server.js
@@ -7,6 +7,10 @@ var myContractJSON  = require(path.join(__dirname, '../Etheruem/build/contracts/
 port = 3000;
 host = '127.0.0.1';
 
+var gameObject = new Object();
+
+initialised = false;
+
 //connect to node
 const provider = new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545");
 const web3 = new Web3(provider);
@@ -34,7 +38,42 @@ CSGOMatches.deployed().then(function(instance) {
             req.on('end', function () {
                 //analyse the payload
                 var obj = JSON.parse(body);
-                console.log("The Player " + obj.player.name + ", is in the state of : " + obj.player.activity);
+
+                if(initialised == false && obj.map == undefined && obj.player.activity == 'menu'){
+                    gameObject.playerId = obj.player.steamid;
+                    initialised = true;
+                }
+
+                if(initialised == true && obj.map != undefined) {
+                    gameObject.mapName = obj.map.name;
+
+                    if(obj.player.steamid == gameObject.playerId && obj.player.match_stats != undefined){
+                        gameObject.kills = obj.player.match_stats.kills;
+                        gameObject.assists = obj.player.match_stats.assists;
+                        gameObject.deaths = obj.player.match_stats.deaths;
+                        gameObject.mvps = obj.player.match_stats.mvps;
+                        gameObject.score = obj.player.match_stats.score;
+                    }
+
+                    if(obj.map.phase == 'gameover'){
+                        gameObject.ctWins = obj.map.team_ct.score;
+                        gameObject.tWins = obj.map.team_t.score;
+
+                        //check if someone won
+                        var tWins = gameObject.tWins;
+                        var ctWins = gameObject.ctWins;
+
+                        var draw = (tWins == 15 && ctWins == 15);
+                        var tWin = tWins == 16;
+                        var ctWin = ctWins == 16;
+
+                        if(draw || tWins || ctWins){
+                            console.log(" --- Game Ended  --- ");
+                        }
+                    }
+                }
+
+                console.log(gameObject);
 
                 res.end( '' );
             });
