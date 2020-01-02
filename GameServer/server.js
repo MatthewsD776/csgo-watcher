b5f3c4bbc51349120afc392d8e4fc0fa75eaa4ad
Darren Matthews <darren.matthews@citrix.com>
2020-01-02 12:31:24 +0000

    Will write what the state of the player is in the console using the payload

--- GameServer/server.js
+++ GameServer/server.js
@@ -1,7 +1,11 @@
 var Web3 = require('web3');
 var path = require('path');
+var http = require('http');
 var contract = require("@truffle/contract");
 var myContractJSON  = require(path.join(__dirname, '../Etheruem/build/contracts/CSGOMatches.json'));
+
+port = 3000;
+host = '127.0.0.1';
 
 //connect to node
 const provider = new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545");
@@ -19,4 +23,24 @@ CSGOMatches.deployed().then(function(instance) {
     });
 
     //set up server to listen for a games
+    server = http.createServer( function(req, res) {
+        if (req.method == 'POST') {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+    
+            var body = '';
+            req.on('data', function (data) {
+                body += data;
+            });
+            req.on('end', function () {
+                //analyse the payload
+                var obj = JSON.parse(body);
+                console.log("The Player " + obj.player.name + ", is in the state of : " + obj.player.activity);
+
+                res.end( '' );
+            });
+        }
+    })
+
+    server.listen(port, host);
+    console.log('Listening at http://' + host + ':' + port);
 });
